e9a7f6cefade464a1cd1fd058bde8d4d501bec4d
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-09-08 22:56:33 +0530

    day 9 javascript basics

---
+++ 3_JavaScript/8_JavaScript Basics/script.js
@@ -0,0 +1,69 @@
+// alert("Hello from script file");
+
+// variables
+
+var a = 10;
+
+// alert(a);
+
+console.log(a);
+
+console.log("Hello hi");
+
+var b = 10.5;
+
+console.log(b);
+
+var c = "Indore is the best"
+
+console.log(c);
+
+// user input
+
+// var x = prompt();
+
+// console.log(x);
+
+var y = prompt("Enter your name: ");
+
+console.log("Hello " + y);
+
+// operators
+
+// arithmatic operators
+
+//  + - * / % 
+
+// boolean operators
+
+// true   - 1
+// false  - 0
+
+// relational operators
+
+// >  <  >=  <=  ==  !=  ===
+
+
+// assignment o.
+
+//  =
+
+// compound o.
+
+//  +=  -=  *=  /=  %= 
+
+var h = 10;
+
+h += 5;   // h = h + 5
+
+console.log(h);
+
+var u = 20;
+
+u %= 3;  // u = u % 3
+
+console.log(u);
+
+
+
+
