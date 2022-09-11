1e35cb6a732fa575825e612183c9a5414bc131a6
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-09-11 13:40:19 +0530

    day 10 javascript conditional statements

---
+++ 3_JavaScript/9_JavaScript Conditional Statements/script.js
@@ -0,0 +1,78 @@
+// conditional Statements
+
+// if and else
+
+// var a = 10;
+
+// if(a > 5){
+//     console.log("Hello");
+// } else{
+//     console.log("Bye");
+// }
+
+
+// if (a == 11){
+//     console.log("one");
+// } else if(a == 5){
+//     console.log("two");
+// } else if(a == 7){
+//     console.log("three");
+// } else{
+//     console.log("Timeout");
+// }
+
+
+// nested if else
+// var a = parseInt(prompt("Enter a Number: "))
+
+// if(a > 5){
+//     console.log(1);
+//     if(a == 7){
+//         console.log(2);
+//     }else{
+//         console.log(3);
+//         if(a == 9){
+//             console.log(4);
+//         }
+//     }
+// } else if(a <= 4){
+//     console.log(5);
+//     if (a==4){
+//         console.log(6);
+//     }
+// } else{
+//     console.log("Finish");
+// }
+
+
+// WAP to enter a number from user and check is it is 
+// greater than 15 or not
+
+// var x = parseInt(prompt("Enter a number: "))
+
+// if(x > 15){
+//     console.log("Greater");
+// } else if(x == 15){
+//     console.log("Equal");
+// } else{
+//     console.log("Smaller");
+// }
+
+// WAP to enter a number and print its absolute value.
+// 5  => 5
+// -7 => 7
+
+
+var x = parseInt(prompt("Enter a number: "))
+
+if (x >= 0){
+    console.log(x);
+} else{
+    console.log(x * (-1));
+}
+
+// WAP to enter a number from user and print its 
+// character format.
+
+// 1 => One
+// 5 => Five
