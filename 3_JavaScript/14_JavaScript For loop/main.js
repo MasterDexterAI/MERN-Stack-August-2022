94bed5d5fcbab6aa0f5a357036c85e032c319e9a
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-09-24 21:58:30 +0530

    day 14 javascript for loop

---
+++ 3_JavaScript/14_JavaScript For loop/main.js
@@ -0,0 +1,51 @@
+// for loop
+
+for( let i = 0; i <= 10 ; i++ ){
+    console.log(i);
+}
+
+// Question 1
+// 1 - 10
+
+// 3 or 5
+
+// 3 + 6 + 5 + 9 = 23
+
+// 15 X
+
+// 1 - 1000
+
+
+// Question 2
+// 2520
+
+// 1 - 10 divisible by all
+
+// ?
+
+// 1 - 20
+
+for(let i = 1; ; i+=20){
+    if (i%2 == 0 && i%3 == 0){
+        console.log(i);
+        break
+    }
+}
+
+// pattern questions
+//
+
+// 11  12  13
+// 21  22  23
+// 31  32  33
+
+// ij
+
+// how to stop line change in console log?
+
+
+// *
+// **
+// ***
+// ****
+// *****
