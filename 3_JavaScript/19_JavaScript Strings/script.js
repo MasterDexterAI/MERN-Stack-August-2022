c34af5320fb16fecca0e4f251354042badfe3218
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-10-17 19:59:14 +0530

    day 19 strings

---
+++ 3_JavaScript/19_JavaScript Strings/script.js
@@ -0,0 +1,73 @@
+// strings
+
+let s = 'indore'
+
+console.log(s);
+
+console.log(typeof s);
+
+let a = 'hello' + 100
+
+console.log(a);
+
+let b =  100 + 'hello'
+
+console.log(b);
+
+// back ticks
+
+let c = `this is a loop
+this is line change ${b}
+`
+
+console.log(c);
+
+// length
+
+let alpha = "india"
+//           01234
+
+// alert(alpha.length)
+
+console.log(alpha[0]);
+
+// TV               -- class
+// - bluetooth
+// - hdmi
+// - usb
+// - wifi
+
+// Actual TV       -- object
+// - Product ID    -- HashCode
+
+// Remote          -- name/reference
+
+// charAt
+
+let goa = "goa is the best"
+
+// alert(goa.charAt(7))
+
+
+// strings are immutable
+
+let f = "indore"
+
+f[0] = "A"  // can not change
+
+console.log(f);
+
+// 1 + 1 = 2  // numbers are immutable
+
+// true // immutable
+
+// questions
+
+// restart
+// resta$t
+
+// level
+// leve$
+
+// abhyudaya
+// abhyud$y$
