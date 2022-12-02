ef2f66dc400ca6aabc2ba93a3c1639c415103612
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-12-02 19:31:32 +0530

    day 30 31 react basics

---
+++ 4_React JS/30_31_React Basics/script.js
@@ -0,0 +1,34 @@
+// bits > numbers > characters > instrcutions > function > class > modules > package > library (react) > framework (MVC / MVT)(Node js)
+
+// JSX - javascript XML
+
+const jsxElement = <h1>I love India</h1>
+
+const header = (
+    <header>
+        <h1>Welcome</h1>
+        <h2>First Session of React</h2>
+        <h3>React library</h3>
+    </header>
+)
+
+//  comment
+
+{
+    /* 
+    <header>
+        <h1>Welcome</h1>
+        <h2>First Session of React</h2>
+        <h3>React library</h3>
+    </header>
+     */
+}
+
+
+// Interview Questions
+
+// What is babel?
+// What is transpiler?
+// What is single page application?
+// What is DOM?
+// What is Virtual DOM?
