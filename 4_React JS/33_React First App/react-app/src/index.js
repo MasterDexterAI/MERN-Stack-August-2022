0e43303727d96c67f3b0ef209ebda1011d80c091
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-12-20 20:50:16 +0530

    day 33 react basics

---
+++ 4_React JS/33_React First App/react-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsxElement = <h1>This is first jsx Element</h1>
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(jsxElement, rootElement);
